799aaee7084540e3c58cd08ab090672204a2a7a2
VoTaiTri <vo.tai.tri@framgia.com>
2015-05-03 15:02:49 +0700

    finish create momodel user use gem devise

--- Gemfile
+++ Gemfile
@@ -27,8 +27,8 @@ gem 'bcrypt', '~> 3.1.7'
 # Use Unicorn as the app server
 gem 'unicorn'
 
-# Use Capistrano for deployment
-# gem 'capistrano-rails', group: :development
+# for model user
+gem 'devise'
 
 group :development, :test do
   # Use sqlite3 as the database for Active Record
---
+++ app/models/user.rb
@@ -0,0 +1,8 @@
+class User < ActiveRecord::Base
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+
+  validates :name, presence: true, length: {maximum: 100}
+  validates :role, presence: true, length: {maximum: 50}
+end
