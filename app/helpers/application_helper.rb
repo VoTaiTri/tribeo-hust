2c77739d483b39f7f0a1cbf2fd0bcb77e123ddae
VoTaiTri <vo.tai.tri@framgia.com>
2015-05-08 10:55:56 +0700

    fix messages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -14,4 +14,22 @@ module ApplicationHelper
     direction = (column == sort_column && sort_direction == "asc") ? "desc" : "asc"
     link_to title, params.merge(sort: column, direction: direction, page: nil), {class: css_class}
   end
+
+  def bootstrap_class_for flash_type
+    _type = flash_type.to_sym
+    case _type
+    when :success
+      "alert-success"
+    when :error
+      "alert-error"
+    when :alert
+      "alert-warning"
+    when :notice
+      "alert-info"
+    when :danger
+      "alert-error"
+    else
+      flash_type.to_s
+    end
+  end
 end
