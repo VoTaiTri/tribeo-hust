606b9918688754db36982b4988c0e483f3ece83e
VoTaiTri <vo.tai.tri@framgia.com>
2015-05-20 16:22:28 +0700

    refactor edit and vn message

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,11 +1,12 @@
 class Course < ActiveRecord::Base
+  DAYNAMES = [['Thứ hai', '2'], ['Thứ ba', '3'], ['Thứ tư', '4'], ['Thứ năm', '5'], ['Thứ sáu', '6']]
+
   belongs_to :subject
   belongs_to :user
   has_many :timetables
 
-  validates :courseID, presence: true
-  validates :term, presence: true
-  validates :subject, presence: true
+  validates_presence_of :courseID, :term, :subject, :timetables,
+              message: I18n.t('activerecord.errors.models.blank')
 
   scope :teach_by, ->user_id {where("user_id = ?", "#{user_id}")}
 
