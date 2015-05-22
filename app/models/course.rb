5eb67af875542f656d5ce4550c62049672d4b1e4
VoTaiTri <vo.tai.tri@framgia.com>
2015-05-22 11:33:54 +0700

    simple validation timetable of course when create and update

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,15 +1,23 @@
 class Course < ActiveRecord::Base
-  DAYNAMES = [['Thứ hai', '2'], ['Thứ ba', '3'], ['Thứ tư', '4'], ['Thứ năm', '5'], ['Thứ sáu', '6']]
-
   belongs_to :subject
   belongs_to :user
-  has_many :timetables
 
-  validates_presence_of :courseID, :term, :subject, :timetables,
+  has_many :timetables, dependent: :destroy
+
+  validates_presence_of :courseID, :term, :subject, :enroll, :max_enroll, :timetables,
               message: I18n.t('activerecord.errors.models.blank')
+  validates_uniqueness_of :courseID, message: I18n.t('activerecord.errors.models.taken') 
+  validate :valid_enroll
 
   scope :teach_by, ->user_id {where(user_id: user_id)}
   scope :need_assign,-> {where("user_confirm = 'rejected' OR division_state= 'spending'")}
 
   accepts_nested_attributes_for :timetables, reject_if: :all_blank, allow_destroy: true
+
+  private
+  def valid_enroll
+    if !enroll.blank? && !max_enroll.blank? && enroll > max_enroll
+      errors.add "Số lượng đăng ký", " phải nhỏ hơn số lượng đăng ký tối đa."
+    end
+  end
 end
--- app/models/timetable.rb
+++ app/models/timetable.rb
@@ -1,3 +1,49 @@
 class Timetable < ActiveRecord::Base
+  DAYNAMES = [['Thứ hai', '2'], ['Thứ ba', '3'], ['Thứ tư', '4'], ['Thứ năm', '5'], ['Thứ sáu', '6']]
+  TIME_START =  [['06:45','06:45'], ['07:35','07:35'], ['08:30','08:30'], ['09:20','09:20'], ['10:15','10:15'], ['11:05','11:05'],
+                 ['12:30','12:30'], ['13:20','13:20'], ['14:15','14:15'], ['15:05','15:05'], ['16:00','16:00'], ['16:50','16:50'],]
+  TIME_FINISH = [['07:30','07:30'], ['08:20','08:20'], ['09:15','09:15'], ['10:05','10:05'], ['11:00','11:00'], ['11:50','11:50'],
+                 ['13:15','13:15'], ['14:05','14:05'], ['15:00','15:00'], ['15:50','15:50'], ['16:45','16:45'],['17:35','17:35'],]
+
+  # attr_accessor :skip_create_room_validation, :skip_update_room_validation
+                 
   belongs_to :course
+
+  validates_presence_of :day, :start_time, :finish_time, :room,
+                        message: I18n.t('activerecord.errors.models.blank')
+  validate :valid_time, :valid_part
+  validate :valid_room_create, on: :create
+  validate :valid_room_update, on: :update
+    
+  scope :with_room, ->room {where("room = ?", "#{room}")}
+  scope :with_day, ->day {where("day = ?", "#{day}")}
+  query = "(start_time <= :start_time AND finish_time >= :finish_time)
+          OR (start_time > :start_time AND start_time < :finish_time)
+          OR (finish_time > :start_time AND finish_time < :finish_time)"
+  scope :filte_timer, ->(start, finish) {where(query, start_time: start, finish_time: finish)}
+
+  private
+  def valid_room_create
+    if Timetable.with_room(room).with_day(day).filte_timer(start_time, finish_time).count > 0
+      errors.add "", "Thời gian, địa điểm học bị trùng"
+    end
+  end
+
+  def valid_room_update
+    if Timetable.with_room(room).with_day(day).filte_timer(start_time, finish_time).count > 1
+      errors.add "", "Thời gian, địa điểm học bị trùng"
+    end
+  end
+
+  def valid_time
+    if !start_time.blank? && !finish_time.blank? && start_time >= finish_time
+      errors.add "Thời gian bắt đầu", " phải trước thời gian kết thúc"
+    end
+  end
+
+  def valid_part
+    if !start_time.blank? && !finish_time.blank? && start_time <= "11:05" && finish_time >= "13:15"
+      errors.add "", "Thời gian học phải trong cùng buổi sáng hoặc buổi chiều"
+    end
+  end
 end
